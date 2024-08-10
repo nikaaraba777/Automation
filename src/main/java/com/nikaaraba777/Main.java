44c7f22086730bfa1553ea0d9b306ab94eabe932
nikaaraba777 <nikaaraba777@yahoo.com>
2024-08-10 19:57:53 -0400

    dd

--- src/main/java/com/nikaaraba777/Main.java
+++ src/main/java/com/nikaaraba777/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("i");
+        System.out.println("new git");
     }
 }
