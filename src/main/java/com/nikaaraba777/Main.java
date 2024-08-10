65e16fb81bbb23a30b6927281022d1a7cf9746ed
nikaaraba777 <nikaaraba777@yahoo.com>
2024-08-10 19:46:50 -0400

    i

--- src/main/java/com/nikaaraba777/Main.java
+++ src/main/java/com/nikaaraba777/Main.java
@@ -3,6 +3,6 @@ package com.nikaaraba777;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hi");
+        System.out.println("i");
     }
 }
